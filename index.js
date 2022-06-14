c90c5c9d05c0b535074b61e8d9b740516f4604f5
Demir Boraj <demirboraj97@gmail.com>
2022-06-14 17:25:13 +0200

    Homework done. Working on challenge

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ let under18Discount = 0.2;
 let over65Discount = 0.4;
 let under18Price = normalPrice - (normalPrice * under18Discount);
 let over65Price = normalPrice - (normalPrice * over65Discount);
+let travelPrice;
+if ( age < 18){
+    travelPrice = under18Price;
+}else if( 18 <= age && age < 65){
+    travelPrice = normalPrice;
+}
+else if( age >= 65){
+    travelPrice = over65Price;
+}
+console.log("Your have to pay: " + travelPrice + "€");
